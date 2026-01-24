8c14ec36d4e0ca6ec4c53869c69b0f52d73cfba2
exu <git@ella.cx>
2026-01-24 16:59:28 -0500

    one link savepoint

--- simulation.py
+++ simulation.py
@@ -6,6 +6,9 @@ physicsClient = p.connect(p.GUI)
 # to disable sidebars:
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+# read in a pyrosim world
+p.loadSDF("box.sdf")
+
 
 for i in range(10000):
     p.stepSimulation()
