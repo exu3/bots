7976d9f24341128fa3426e22f3b3aa06d9da172a
exu <git@ella.cx>
2026-02-02 18:34:55 -0500

    Savepoint
    stopped at A two-link, one-joint robot.
     as in, i should start there next time

--- generate.py
+++ generate.py
@@ -1,7 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-
 x = 0
 y = 0
 z = .5
@@ -10,29 +8,20 @@ length = 1
 width = 1
 height = 1
 
-# pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-# pyrosim.Send_Cube(name="Box2", pos=[
-#                   x + length, y, z + height], size=[length, width, height])
 
-# nine towers, ohyeah
-for row in range(3):
-    for col in range(3):
-        x = row
-        y = col
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(
+        name="Box", pos=[-2, -2, z], size=[length, width, height])
+    pyrosim.End()
 
-        length = 1
-        width = 1
-        height = 1
 
-        current_z = height / 2
-        for i in range(10):
-            pyrosim.Send_Cube(name=f"Box{row}{col}{i}",
-                              pos=[x, y, current_z],
-                              size=[length, width, height])
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[
+                      length, width, height])
+    pyrosim.End()
 
-            current_z += height
-            length *= 0.9
-            width *= 0.9
-            height *= 0.9
 
-pyrosim.End()
+Create_World()
+Create_Robot()
--- simulation.py
+++ simulation.py
@@ -12,9 +12,10 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
 # read in a pyrosim world
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 
 
 for i in range(10000):
